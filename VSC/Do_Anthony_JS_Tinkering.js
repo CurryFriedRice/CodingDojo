982edc2eeae9904b4ca6c64cedaabced457545c6
BLD\FriedRice <anthonyweido@gmail.com>
2021-12-02 22:01:16 -0800

    Updated Zip and Script to follow direction of var 'childHeight' and console commands

--- VSC/Do_Anthony_JS_Tinkering.js
+++ VSC/Do_Anthony_JS_Tinkering.js
@@ -1,12 +1,14 @@
+var childHeight = 54
 var heightReq = 52
 
-function checkHeight(height)
+function displayIfChildIsAbleToRideTheRollerCoaster(height)
 {
     if(height < heightReq) console.log("Sorry kiddo. Maybe next Year");
     else console.log("Get on that ride, kiddo!");
 }
 
-function checkHeightOneLine(height)
+//I wrote this one first
+function displayIfChildIsAbleToRideTheRollerCoasterOneLine(height)
 {
     height > heightReq ? console.log("Get on that ride, kiddo!") : console.log("Sorry kiddo. Maybe next Year");
 }
