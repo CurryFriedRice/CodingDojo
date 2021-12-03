b37da41a66e2eb059df9a5d61944f0e4875ee492
BLD\FriedRice <anthonyweido@gmail.com>
2021-12-02 16:28:30 -0800

    Wrote the JS Exercises and also zipped it for upload

---
+++ VSC/Do_Anthony_JSPredict.js
@@ -0,0 +1,13 @@
+//Predict 1: No input Data
+//Output: I was Born in 1980
+
+//Predict 2: birthYearInput = 1980
+//Output: I was born in 1980
+
+//More outputs means I changed the format 
+//Predict 3: var num1 = 10 and var num2 = 20
+//=====Output Below=====
+//Summing Numbers!
+//num1 is: 10
+//num2 is: 20
+//30
