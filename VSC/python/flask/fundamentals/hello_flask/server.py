ca196c1c78659c964252bcc7241ce07211591ae6
BLD\FriedRice <anthonyweido@gmail.com>
2022-01-12 21:58:28 -0800

    playground in a single template

--- VSC/python/flask/fundamentals/hello_flask/server.py
+++ VSC/python/flask/fundamentals/hello_flask/server.py
@@ -1,18 +1,21 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 @app.route('/') # targets the function immediately following
 def hello_world():
-    return'Hello_World'
+    return render_template("index.html")
 
 @app.route("/success")
 def success():
     return "SUCCESS"
 
 
-@app.route('/hello/<string:vars>/<int:count>')
-def hello(vars, count):
-    return f"Hello {vars}"*count
+@app.route('/hello/<string:val>/<int:count>')
+def hello(val, count):
+    return render_template("repeat.html", val=val, count=count)
+
 
 
 if __name__=="__main__":
