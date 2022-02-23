28956e7a40eda75c116f50de6b9c10f10b19c930
BLD\FriedRice <anthonyweido@gmail.com>
2022-02-23 14:06:49 -0800

    Adding purchases to customers

---
+++ VSC/C#_ORMs/e_commerce/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using e_commerce.Models;
+
+using Stripe;
+
+namespace e_commerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+    
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Products")]
+        public IActionResult Index()
+        {
+            InvoiceService service = new InvoiceService();
+            var requestOptions = new RequestOptions
+            {
+                ApiKey = "sk_test_51KW5uZFBXJzgkFoluhW0nVcnAPNkdE5sfHkMiVIDzMBzNbDY0G1ppEHlWpAEzWmWDW6vV27xYJldoLvR5DQY0kFM00GDXz75Oq"
+            };
+            StripeList<Invoice> invoices = service.List(null, requestOptions);
+            //Console.WriteLine(invoices);
+            ViewBag.Invoices = invoices;
+            ViewBag.Customers = new CustomerService().List(null,requestOptions);
+            ViewBag.Products = new ProductService().List(null, requestOptions);
+            return View();
+        }
+        
+        [HttpPost("Products/add")]
+        public IActionResult Add(AddOrder newOrder)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51KW5uZFBXJzgkFoluhW0nVcnAPNkdE5sfHkMiVIDzMBzNbDY0G1ppEHlWpAEzWmWDW6vV27xYJldoLvR5DQY0kFM00GDXz75Oq";
+
+            var prodService = new ProductService();
+            var prod = prodService.Get(newOrder.ProductID);
+            Console.WriteLine(prod);
+  
+            var custService = new CustomerService();
+            var cust = custService.Get(newOrder.CustomerID);
+           
+            var priceService = new PriceService();
+            var allPrices = priceService.List();
+
+            var iiOptions = new InvoiceItemCreateOptions
+            {
+                Customer =  newOrder.CustomerID,
+                Quantity = newOrder.Quantity,
+                Description = prod.Name,
+                Price = allPrices.FirstOrDefault(price => price.ProductId == newOrder.ProductID).Id
+            };
+            var iiService = new InvoiceItemService();
+            iiService.Create(iiOptions);
+
+            var invoiceOptions = new InvoiceCreateOptions
+            {
+                Customer = newOrder.CustomerID,
+            };
+            var service = new InvoiceService();
+            service.Create(invoiceOptions);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
