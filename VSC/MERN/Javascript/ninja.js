da99c59d56221b55f13cad578c5df1100940b2f4
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-07 17:10:17 -0800

    Ninja Class

---
+++ VSC/MERN/Javascript/ninja.js
@@ -0,0 +1,23 @@
+class Ninja
+{
+    constructor(name, health = 100, speed = 3, strength = 3 )
+    {
+        this.name = name;
+        this.health= health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    
+    sayName = () => console.log(this.name);
+    showStats = () => 
+    {
+        console.log(`STR: ${this.strength} | SPD ${this.speed}`)
+    }
+    drinkSake = () => this.health += 10;
+    
+}
+
+const ninja = new Ninja("Hayabusa");
+
+ninja.sayName();
+ninja.showStats();
