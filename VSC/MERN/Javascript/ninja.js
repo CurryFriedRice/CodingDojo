0487e3b85691f3da5f73c0340238ff54fe3d1dbc
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-07 17:26:21 -0800

    Super NInja

--- VSC/MERN/Javascript/ninja.js
+++ VSC/MERN/Javascript/ninja.js
@@ -11,14 +11,36 @@ class Ninja
     sayName = () => console.log(this.name);
     showStats = () => 
     {
-        console.log(`HP: ${this.health} | STR: ${this.strength} | SPD ${this.speed}`)
+        console.log(`Name: ${this.name} | HP: ${this.health} | STR: ${this.strength} | SPD ${this.speed}`)
     }
     drinkSake = () => this.health += 10;
     
 }
 
+class Sensei extends Ninja
+{
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10)
+    {
+        super(name, health, speed, strength);
+        this.wisdom = wisdom;
+        this.quotes = 
+            [
+            "What one programmer can do in one month, two programmers can do in two months",
+            "Hours of coding can save weeks of planning",
+            "Go to the store and buy a carton of milk. If there are apples buy two",
+            "What is a programmer? AN INCOMPLETE LIBRARY OF CODE?",
+            "If you understand maybe you should get Arrays"
+            ]
+    }
+
+    speakWisdom = () => {console.log(this.quotes[Math.floor(Math.random()*this.quotes.length)])}
+} 
+
 const ninja = new Ninja("Hayabusa");
+const superNinja = new Sensei("Master Splinter");
 
 ninja.sayName();
 // ninja.drinkSake();
 ninja.showStats();
+superNinja.showStats();
+superNinja.speakWisdom();
