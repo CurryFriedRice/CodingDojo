0712389e9f24163cb2b38d069038e176006a399f
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-08 17:52:09 -0800

    More Forms!

---
+++ VSC/MERN/React/More_Forms/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import PersonCardComponent from "./Components/PersonCardComponent.js"
+import UserForm from "./Components/UserForm.jsx"
+import User from './JS/User';
+
+function App() {
+  let userList = []
+  userList.push(new User("Jane","Doe",45, "Brown"))
+  userList.push(new User("John", "Smith", 88,"Brown"))
+  userList.push(new User("Millard","Fillmore", 50, "Brown"))
+  userList.push(new User("Maria", "Smith", 62, "Brown"))
+
+  let UserJSX = [];
+  for (const item of userList) {
+    // UserJSX.push(<PersonCardComponent firstName={item.firstName} lastName={item.lastName} age={item.age} hairColor={item.hairColor}/>)
+    UserJSX.push(<PersonCardComponent user={item}/>)
+  } 
+
+
+  return (
+    <div className="App">
+      <UserForm/>
+      
+      {/* <PersonCardComponent firstName={userList[0].firstName} lastName={userList[0].lastName} age={userList[0].age} hairColor={userList[0].hairColor}/> */}
+      
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ VSC/MERN/React/More_Forms/JS/User.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * @summary A basic user for for Prop it up
+ * @param {string} firstName;
+ * @param {string} lastName;
+ * @param {int} age;
+ * @param {string} color;
+ */
+class User
+{
+    constructor(firstName, lastName, age, hairColor)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hairColor = hairColor;
+    }
+}
+
+
+
+export default User;
