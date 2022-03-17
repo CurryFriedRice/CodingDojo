7ddf8a9502410b7987d0e9d605c9c1378c0175c3
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-17 09:51:43 -0700

    Jokes Assignment

---
+++ VSC/MERN/MongoDB/Jokes/server/routes/joke.routes.js
@@ -0,0 +1,13 @@
+// import JokeController from "../controllers/joke.controller"
+
+
+const JokeController = require("../controllers/joke.controller");
+
+
+module.exports = app => {
+  app.get("/api/jokes/", JokeController.findAllJokes);
+  app.get("/api/jokes/:id", JokeController.findOneSingleJoke);
+  app.put("/api/jokes/update/:id", JokeController.updateExistingJoke);
+  app.post("/api/jokes/new", JokeController.createNewJoke);
+  app.delete("/api/jokes/delete/:id", JokeController.deleteAnExistingJoke);
+};
