256443240d726dcd735a9c7709b442c6a0b7263b
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-18 10:59:26 -0700

    Added Validation

--- VSC/MERN/MongoDB/Jokes/server/models/joke.model.js
+++ VSC/MERN/MongoDB/Jokes/server/models/joke.model.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 
 //The model for a joke so each Joke that we make is consistent
 const JokeSchema = new mongoose.Schema({
-	setup: String,
-	punchline: String
+	setup: {
+		type: String,
+		required: [true, "A Setup to your joke is required"],
+		minlength:[3, "Setup must be at least 3 characters long"]
+	},
+	punchline:
+	{
+		type: String,
+		required: [true, "A Punchline to your joke is required"],
+		minlength:[10, "Punchline must be at least 3 characters long"]
+	}
 });
 
 //Make a model called Joke that has uses JokeSchema as the document model.
